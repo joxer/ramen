5278b29045ccf8b798d356f56ce91a6955a3e29d
joxer <kell.92.k@gmail.com>
2009-03-12 15:30:59 +0100

    yo

---
+++ servers/Handler/varius_webrick.rb
@@ -0,0 +1,59 @@
+
+
+include WEBrick
+
+
+
+class MethodHandler < HTTPServlet::AbstractServlet
+
+  def do_GET(req, res)
+    
+    params = QueryString.parse( req.query_string )
+    
+    con = Controller.new
+
+    res.body = con.send params['method'].to_sym
+    
+    res['Content-Type'] = 'text/html'
+
+
+  end
+
+
+end
+
+
+class Socket_Handler < HTTPServlet::AbstractServlet
+
+  def do_GET(req, res)
+
+    params = QueryString.parse(req.query_string)
+
+    sock = Socket.new(params['url'])
+
+    res.body =  sock.send params['method'].to_sym
+
+    res['Content-Type'] = 'text/html'
+
+
+  end
+
+end
+
+
+class File_Handler < HTTPServlet::AbstractServlet
+
+
+  def do_GET(req, res)
+
+    params = QueryString.parse(req.query_string)
+
+    file = File.new(params['file'])
+
+    res.body = file.send params['method'].to_sym
+
+    res['Content-Type'] = 'text/html'
+
+  end
+
+end
